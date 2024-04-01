b32f7cf4c651b1fbc5df8c43495c49cd339da3ff
Elham Sadeghi <100062888+Esadeghii@users.noreply.github.com>
2024-04-01 11:15:03 -0400

    sample-vis

--- sampleVisualizations.py
+++ sampleVisualizations.py
@@ -5,72 +5,73 @@
 # @author: farihamoomtaheen
 # """
 
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import plotly.express as px
-# from matplotlib import path
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+from matplotlib import path
+import sampleSequences as seq
 
-# df = pd.read_csv("data-for-sampling/past-samples-with-info/samples-model-2/detailed-sequences-model-2", index_col = 0)
+df = pd.read_csv("data-for-sampling/past-samples-with-info/samples-1702884742.4237525-GREEN-DEC2023/detailed-sequences", index_col = 0)
 
 
-# def Linechart(x, y1, y2, dimName):
+def Linechart(x, y1, y2, dimName):
 
-# # compare z0,z1 before and after re-encoding. Linechart
-#     plt.figure()
-#     plt.plot(x, y1, label= dimName + ' Generated')  
-#     plt.plot(x, y2, label= dimName + ' Encoded')
+# compare z0,z1 before and after re-encoding. Linechart
+    plt.figure()
+    plt.plot(x, y1, label= dimName + ' Generated')  
+    plt.plot(x, y2, label= dimName + ' Encoded')
     
-#     plt.legend(loc="upper left")
-#     plt.xlabel("No of Samples")
-#     plt.ylabel(dimName)
-#     plt.title(dimName + " Comparison-Linechart")
+    plt.legend(loc="upper left")
+    plt.xlabel("No of Samples")
+    plt.ylabel(dimName)
+    plt.title(dimName + " Comparison-Linechart")
     
-#     plt.show()
+    plt.show()
     
-# def Scatter(x, y1, y2, dimName):
+def Scatter(x, y1, y2, dimName):
     
-# # compare z0,z1 before and after re-encoding. Scatter
-#     plt.figure()    
-#     plt.scatter(x,y1,c='blue', label= dimName + ' Generated')
-#     plt.scatter(x,y2,c='red', label= dimName + ' Encoded')
+# compare z0,z1 before and after re-encoding. Scatter
+    plt.figure()    
+    plt.scatter(x,y1,c='blue', label= dimName + ' Generated')
+    plt.scatter(x,y2,c='red', label= dimName + ' Encoded')
     
-#     plt.legend(loc="upper left")
-#     plt.xlabel("No of Samples")
-#     plt.ylabel(dimName)
-#     plt.title(dimName + " Comparison-Scatterplot")
+    plt.legend(loc="upper left")
+    plt.xlabel("No of Samples")
+    plt.ylabel(dimName)
+    plt.title(dimName + " Comparison-Scatterplot")
     
-#     plt.show()
+    plt.show()
     
-# # Wavelength plots
-# x = np.linspace(1, len(df), len(df))
-# y1 = df['Wavelen Generated']
-# y2 = df['Wavelen Encoded']
-# dimension = 'Wavelength'
+# Wavelength plots
+x = np.linspace(1, len(df), len(df))
+y1 = df['Wavelen Generated']
+y2 = df['Wavelen Encoded']
+dimension = 'Wavelength'
 
-# Linechart(x,y1,y2, dimension)
-# Scatter(x, y1, y2, dimension)
+Linechart(x,y1,y2, dimension)
+Scatter(x, y1, y2, dimension)
 
 
-# # LII plots
-# y1 = df['LII Generated']
-# y2 = df['LII Encoded']
-# dimension = 'LII'
+# LII plots
+y1 = df['LII Generated']
+y2 = df['LII Encoded']
+dimension = 'LII'
 
-# Linechart(x, y1, y2, dimension)
-# Scatter(x, y1, y2, dimension)
+Linechart(x, y1, y2, dimension)
+Scatter(x, y1, y2, dimension)
 
-# # fig = px.scatter(x, y1, color_discrete_sequence=['red'])
-# # fig = px.scatter(x, y2, color_discrete_sequence=['blue'])
-# # fig.show()
+# fig = px.scatter(x, y1, color_discrete_sequence=['red'])
+# fig = px.scatter(x, y2, color_discrete_sequence=['blue'])
+# fig.show()
 
-# #compare difference in z0 before and after re-encoding. 
-# y = np.subtract(y1,y2)
-# plt.plot(x,y)
-# # plt.show()
-
-# plt.scatter(x,y, c='red')
+#compare difference in z0 before and after re-encoding. 
+y = np.subtract(y1,y2)
+plt.plot(x,y)
 # plt.show()
+
+plt.scatter(x,y, c='red')
+plt.show()
 
 
 def process_sequences_wavelength_lii(base_data_path: str):
@@ -104,7 +105,8 @@ def write_clean_data(base_data_csv: str, processed_data_file: str, model: str, g
         for i, matrix in enumerate(mean):
             f.write(f"{sequence_data[i]},{wavelength_data[i]},{lii_data[i]},{matrix[0]},{matrix[1]}\n")
 
-write_clean_data('cleandata.csv', 'data-for-sampling/processed-data-files/clean-data-base.npz', 'all-results/1-18-22-res/models/a0.005lds19b0.008g1.0d1.0h13.pt', 'data-info-model-4.csv')
+#write_clean_data('data-for-sampling/past-samples-with-info/samples-1702884742.4237525-GREEN-DEC2023/generated-sequences.csv', 'data-for-sampling/past-samples-with-info/samples-1702884742.4237525-GREEN-DEC2023/generated-sequences--1702884750.56106.npz', 'models/weighted/a0.007lds15b0.007g1d1h13.pt', 'reencoded-data-info-model-4.csv')
+write_clean_data('data-and-cleaning/cleandata.csv', 'data-for-sampling/processed-data-files/clean-data-base-1702887664.4233212.npz', 'models/weighted/a0.007lds15b0.007g1d1h13.pt', 'data-info-model-4.csv')
 
 
 
